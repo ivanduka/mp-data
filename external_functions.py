5c0a6c1a1cc97a56ba751466780df9017e8f2b9f
rodijann <jrodioukova@gmail.com>
2020-04-03 12:14:55 -0600

    improved title extraction functions

--- external_functions.py
+++ external_functions.py
@@ -5,6 +5,7 @@ import re
 from bs4 import BeautifulSoup
 
 save_dir = '//luxor/data/branch/Environmental Baseline Data/Version 4 - Final/Saved2/'
+# save_dir = 'C:/Users/rodijann/RegDocs/Saved/'
 
 # regex expressions needed for the extractions
 empty_line = r'<\/p>\s*<p ?\/?>'
@@ -12,13 +13,59 @@ whitespace = r'\s+'  # all white space
 punctuation = r'[^\w\s]'  # punctuation (not letter or number)
 figure = r'(?im)(^Figure .*?\n?.*?)\.{2,}(.*)'
 table = r'(?im)(^Table (?!of contents?).*?\n?.*?)\.{2,}(.*)'
-load_pickles = 1  # if document pickles need to be loaded to projects (only need to do once)
+
+
+def fig_checker(doc_text, doc_text_rotated, doc_id, toc_id, toc_page, word1_rex, word2_rex, s2_rex):
+    page_list = []
+    # check unrotated
+    soup = BeautifulSoup(doc_text, 'lxml')
+    pages = soup.find_all('div', attrs={'class': 'page'})
+    for page_num, page in enumerate(pages):
+        text_clean = re.sub(whitespace, ' ', page.text)
+        # text_clean = re.sub(punctuation, ' ', text_clean)
+        if re.search(word2_rex, text_clean) and re.search(s2_rex, text_clean):
+            if (doc_id != toc_id) or (page_num != toc_page):
+                page_list.append(page_num)
+
+    # check rotated
+    soup = BeautifulSoup(doc_text_rotated, 'lxml')
+    pages = soup.find_all('div', attrs={'class': 'page'})
+    for page_num, page in enumerate(pages):
+        text_clean = re.sub(whitespace, ' ', page.text)
+        # text_clean = re.sub(punctuation, ' ', text_clean)
+        if re.search(word2_rex, text_clean) and re.search(s2_rex, text_clean):
+            if (doc_id != toc_id) or (page_num != toc_page):
+                page_list.append(page_num)
+    return page_list
+
+
+def table_checker(doc_text, doc_text_rotated, doc_id, toc_id, toc_page, s1_rex, s2_rex):
+    page_list = []
+    # check unrotated
+    soup = BeautifulSoup(doc_text, 'lxml')
+    pages = soup.find_all('div', attrs={'class': 'page'})
+    for page_num, page in enumerate(pages):
+        text_clean = re.sub(whitespace, ' ', page.text)
+        # text_clean = re.sub(punctuation, ' ', text_clean)
+        if re.search(s1_rex, text_clean) and re.search(s2_rex, text_clean):
+            if (doc_id != toc_id) or (page_num != toc_page):
+                page_list.append(page_num)
+
+    # check rotated
+    soup = BeautifulSoup(doc_text_rotated, 'lxml')
+    pages = soup.find_all('div', attrs={'class': 'page'})
+    for page_num, page in enumerate(pages):
+        text_clean = re.sub(whitespace, ' ', page.text)
+        # text_clean = re.sub(punctuation, ' ', text_clean)
+        if re.search(s1_rex, text_clean) and re.search(s2_rex, text_clean):
+            if (doc_id != toc_id) or (page_num != toc_page):
+                page_list.append(page_num)
+    return page_list
+
 
 # function that takes ID of project, and finds locations of all the tables from that projects' TOC
 # saves result to save_dir folder
-
-
-def get_titles(project):
+def get_titles_tables(project):
     buf = StringIO()
     with redirect_stdout(buf), redirect_stderr(buf):
         try:
@@ -29,110 +76,176 @@ def get_titles(project):
             df_tables['location_Page'] = None
             df_tables['count'] = 0
             df_project = pd.read_csv(save_dir + 'project_' + project + '.csv', encoding='utf-8-sig', index_col='DataID')
+            df_project['Text_rotated'].fillna('', inplace=True)
 
+            prev_id = 0
             for index, row in df_tables.iterrows():
-                if project == row['Project']:
-                    title = row['Name']
-                    # print(title)
-                    c = title.count(' ')
-                    if c >= 2:
-                        word1, word2, s2 = title.split(' ', 2)
+                title = row['Name']
+                # print(title)
+                c = title.count(' ')
+                if c >= 2:
+                    word1, word2, s2 = title.split(' ', 2)
+                else:
+                    word1, word2 = title.split(' ', 1)
+                    s2 = ''
+                word1_rex = r'(?i)\b' + word1 + r'\s'
+                word2_rex = r'(?i)\b' + word2
+                s1_rex = r'(?i)\b' + word1 + r'\s' + word2
+
+                s2 = re.sub(punctuation, ' ', s2)
+                s2 = re.sub(whitespace, ' ', s2)  # remove whitespace
+                s2_rex = r'(?i)\b'
+                for s in s2.split(' '):
+                    s2_rex = s2_rex + r'[^\w]*' + s
+                s2_rex = s2_rex + r'\b'
+                toc_id = row['DataID']
+                project = row['Project']
+                toc_page = row['TOC_Page']
+
+                id_list = []
+                page_list = []
+                count = 0
+
+                # first try previos id if exists
+                if prev_id > 0:
+                    doc_id = prev_id
+                    doc_text = df_project.loc[doc_id, 'Text']
+                    doc_text_rotated = df_project.loc[doc_id, 'Text_rotated']
+                    page_list = table_checker(doc_text, doc_text_rotated, doc_id, toc_id, toc_page, s1_rex, s2_rex)
+                    if len(page_list) > 0:
+                        id_list = [doc_id]
+                        count = len(page_list)
+
+                # if didn't find try TOC document
+                if count == 0:
+                    doc_id = toc_id
+                    doc_text = df_project.loc[doc_id, 'Text']
+                    doc_text_rotated = df_project.loc[doc_id, 'Text_rotated']
+                    page_list = table_checker(doc_text, doc_text_rotated, doc_id, toc_id, toc_page, s1_rex, s2_rex)
+                    if len(page_list) > 0:
+                        id_list = [doc_id]
+                        count = len(page_list)
+                        prev_id = doc_id
                     else:
-                        word1, word2 = title.split(' ', 1)
-                        s2 = ''
-                    word1_rex = r'(?i)\b' + word1 + r'\s'
-                    word2_rex = r'(?i)\b' + word2 + r'\s'
-                    s2 = re.sub(punctuation, ' ', s2)
-                    s2 = re.sub(whitespace, ' ', s2)  # remove whitespace
-                    s2_rex = r'(?i)\b'
-                    for s in s2.split(' '):
-                        s2_rex = s2_rex + r'[^\w]*' + s
-                    s2_rex = s2_rex + r'\b'
-                    toc_id = row['DataID']
-                    project = row['Project']
-                    toc_page = row['TOC_Page']
+                        id_list = []
+                        count = 0
 
-                    id_list = []
-                    page_list = []
-                    count = 0
+                # if fig still not found, go through all docs in this project and try to find the doc there
+                if count == 0:
+                    for doc_id, doc in df_project.iterrows():
+                        if (doc_id != toc_id) and (doc_id != prev_id):
+                            doc_text = df_project.loc[doc_id, 'Text']
+                            doc_text_rotated = df_project.loc[doc_id, 'Text_rotated']
+                            p_list = table_checker(doc_text, doc_text_rotated, doc_id, toc_id, toc_page, s1_rex, s2_rex)
+                            if len(p_list) > 0:
+                                id_list.append(doc_id)
+                                page_list.extend(p_list)
+                                count += len(p_list)
+                    if len(id_list) == 1:
+                        prev_id = id_list[0]
 
-                    # first open up same doc (same as TOC) and try to find the fig there
-                    soup = BeautifulSoup(df_project.loc[row['DataID'], 'Text'], 'lxml')
-                    pages = soup.find_all('div', attrs={'class': 'page'})
-                    for page_num, page in enumerate(pages):
-                        text_clean = re.sub(whitespace, ' ', page.text)
-                        text_clean = re.sub(punctuation, ' ', text_clean)
-                        # if re.search(s1_rex, page.text) and re.search(s2_rex, re.sub(whitespace, ' ', page.text)):
-                        if re.search(
-                                word1_rex, page.text) and re.search(
-                                word2_rex, page.text) and re.search(
-                                s2_rex, text_clean):
-                            if page_num != toc_page:
-                                id_list = [row['DataID']]
-                                page_list.append(page_num)
-                                count += 1
-
-                    # if fig not found, try in rotated document
-                    if count == 0:
-                        soup = BeautifulSoup(df_project.loc[row['DataID'], 'Text_rotated'], 'lxml')
-                        pages = soup.find_all('div', attrs={'class': 'page'})
-                        for page_num, page in enumerate(pages):
-                            text_clean = re.sub(whitespace, ' ', page.text)
-                            text_clean = re.sub(punctuation, ' ', text_clean)
-                            # if re.search(s1_rex, page.text) and re.search(s2_rex, re.sub(whitespace, ' ', page.text)):
-                            if re.search(
-                                    word1_rex, page.text) and re.search(
-                                    word2_rex, page.text) and re.search(
-                                    s2_rex, text_clean):
-                                if page_num != toc_page:
-                                    id_list = [row['DataID']]
-                                    page_list.append(page_num)
-                                    count += 1
-
-                    # if fig still not found, go through all docs in this project and try to find the doc there
-                    if count == 0:
-                        for doc_id, doc in df_project.iterrows():
-                            soup = BeautifulSoup(doc['Text'], 'lxml')
-                            pages = soup.find_all('div', attrs={'class': 'page'})
-                            for page_num, page in enumerate(pages):
-                                text_clean = re.sub(whitespace, ' ', page.text)
-                                text_clean = re.sub(punctuation, ' ', text_clean)
-                                # if re.search(s1_rex, page.text) and re.search(s2_rex, re.sub(whitespace, ' ', page.text)):
-                                if re.search(
-                                        word1_rex, page.text) and re.search(
-                                        word2_rex, page.text) and re.search(
-                                        s2_rex, text_clean):
-                                    if (doc_id != toc_id) or (page_num != toc_page):
-                                        if doc_id not in id_list:
-                                            id_list.append(doc_id)
-                                        page_list.append(page_num)
-                                        count += 1
-
-                    # if still not found, go through all rotated figs and try there
-                    if count == 0:
-                        for doc_id, doc in df_project.iterrows():
-                            soup = BeautifulSoup(doc['Text_rotated'], 'lxml')
-                            pages = soup.find_all('div', attrs={'class': 'page'})
-                            for page_num, page in enumerate(pages):
-                                text_clean = re.sub(whitespace, ' ', page.text)
-                                text_clean = re.sub(punctuation, ' ', text_clean)
-                                # if re.search(s1_rex, page.text) and re.search(s2_rex, re.sub(whitespace, ' ', page.text)):
-                                if re.search(
-                                        word1_rex, page.text) and re.search(
-                                        word2_rex, page.text) and re.search(
-                                        s2_rex, text_clean):
-                                    if (doc_id != toc_id) or (page_num != toc_page):
-                                        if doc_id not in id_list:
-                                            id_list.append(doc_id)
-                                        page_list.append(page_num)
-                                        count += 1
-
-                    df_tables.loc[index, 'location_DataID'] = str(id_list).replace('[', '').replace(']', '').strip()
-                    df_tables.loc[index, 'location_Page'] = str(page_list).replace('[', '').replace(']', '').strip()
-                    df_tables.loc[index, 'count'] = count
+                df_tables.loc[index, 'location_DataID'] = str(id_list).replace('[', '').replace(']', '').strip()
+                df_tables.loc[index, 'location_Page'] = str(page_list).replace('[', '').replace(']', '').strip()
+                df_tables.loc[index, 'count'] = count
             df_tables.to_csv(save_dir + project + '-final_tables.csv', index=False, encoding='utf-8-sig')
             print(f"Finished {project}")
         except Exception as e:
             print(e)
         finally:
             return buf.getvalue()
+
+
+def get_titles_figures(project):
+    buf = StringIO()
+    with redirect_stdout(buf), redirect_stderr(buf):
+        try:
+            print(f"Starting {project}")
+            df_figs = pd.read_csv(save_dir + 'all_figs.csv', encoding='utf-8-sig')
+            df_figs = df_figs[df_figs['Project'] == project]  # filter out just current project
+            df_figs['location_DataID'] = None
+            df_figs['location_Page'] = None
+            df_figs['count'] = 0
+            df_project = pd.read_csv(save_dir + 'project_' + project + '.csv', encoding='utf-8-sig', index_col='DataID')
+            df_project['Text_rotated'].fillna('', inplace=True)
+
+            prev_id = 0
+            for index, row in df_figs.iterrows():
+                title = row['Name']
+                # print(title)
+                c = title.count(' ')
+                if c >= 2:
+                    word1, word2, s2 = title.split(' ', 2)
+                else:
+                    word1, word2 = title.split(' ', 1)
+                    s2 = ''
+                word2 = re.sub('[^a-zA-Z0-9]', '[^a-zA-Z0-9]', word2)
+                word1_rex = r'(?i)\b' + word1 + r'\s'
+                word2_rex = r'(?i)\b' + word2
+
+                # s2 = re.sub(punctuation, ' ', s2)
+                s2 = re.sub(whitespace, ' ', s2)  # remove whitespace
+                s2 = re.sub(punctuation, '.*', s2)
+
+                s2_rex = r'(?i)\b'
+                for s in s2.split(' '):
+                    s2_rex = s2_rex + r'[^\w]*' + s
+                s2_rex = s2_rex + r'\b'
+                toc_id = row['DataID']
+                toc_page = row['TOC_Page']
+
+                id_list = []
+                page_list = []
+                count = 0
+
+                # first try previos id if exists
+                if prev_id > 0:
+                    doc_id = prev_id
+                    doc_text = df_project.loc[doc_id, 'Text']
+                    doc_text_rotated = df_project.loc[doc_id, 'Text_rotated']
+                    page_list = fig_checker(doc_text, doc_text_rotated, doc_id, toc_id, toc_page, word1_rex,
+                                            word2_rex, s2_rex)
+                    if len(page_list) > 0:
+                        id_list = [doc_id]
+                        count = len(page_list)
+
+                # if didn't find try TOC document
+                if count == 0:
+                    doc_id = toc_id
+                    doc_text = df_project.loc[doc_id, 'Text']
+                    doc_text_rotated = df_project.loc[doc_id, 'Text_rotated']
+                    page_list = fig_checker(doc_text, doc_text_rotated, doc_id, toc_id, toc_page, word1_rex, word2_rex,
+                                            s2_rex)
+                    if len(page_list) > 0:
+                        id_list = [doc_id]
+                        count = len(page_list)
+                        prev_id = doc_id
+                    else:
+                        id_list = []
+                        count = 0
+
+                # if fig still not found, go through all docs in this project and try to find the doc there
+                if count == 0:
+                    for doc_id, doc in df_project.iterrows():
+                        if (doc_id != toc_id) and (doc_id != prev_id):
+                            doc_text = df_project.loc[doc_id, 'Text']
+                            doc_text_rotated = df_project.loc[doc_id, 'Text_rotated']
+                            p_list = fig_checker(doc_text, doc_text_rotated, doc_id, toc_id, toc_page, word1_rex,
+                                                 word2_rex, s2_rex)
+                            if len(p_list) > 0:
+                                id_list.append(doc_id)
+                                page_list.extend(p_list)
+                                count += len(p_list)
+                    if len(id_list) == 1:
+                        prev_id = id_list[0]
+
+                df_figs.loc[index, 'location_DataID'] = str(id_list).replace('[', '').replace(']', '').strip()
+                df_figs.loc[index, 'location_Page'] = str(page_list).replace('[', '').replace(']', '').strip()
+                df_figs.loc[index, 'count'] = count
+
+            df_figs.to_csv(save_dir + project + '-final_figs.csv', index=False, encoding='utf-8-sig')
+            print(f"Finished {project}")
+        except Exception as e:
+            print(e)
+        finally:
+            return buf.getvalue()
+            # return True
--- table_titles.py
+++ table_titles.py
@@ -2,17 +2,17 @@ import pandas as pd
 import re
 from bs4 import BeautifulSoup
 from multiprocessing import Pool
-from external_functions import get_titles, save_dir
+from external_functions import get_titles_tables, save_dir
 from pathlib import Path
 
 if __name__ == "__main__":
     # get all project IDs (in a list)
-    projects_path = Path(r'\\luxor\data\branch\Environmental Baseline Data\Version 4 - Final\Indices\Index 2 - PDFs for Major Projects with ESAs.xlsx')
+    projects_path = Path(
+        r'\\luxor\data\branch\Environmental Baseline Data\Version 4 - Final\Indices\Index 2 - PDFs for Major Projects with ESAs.xlsx')
     all_projects = pd.read_excel(projects_path)
     projects = all_projects['Hearing order'].unique()
     # print(projects)
-    # projects = ['GH-1-2003', 'HW-001- # to test
-
+    projects = ['OH-002-2016']
 
     # assuming saved_dir has all the project csvs already, and has the all_tables file in it already
     # go through every table and find it in some document and record ID and real page num
@@ -21,12 +21,12 @@ if __name__ == "__main__":
     results = []
 
     # Sequential Mode - if using, comment out the multiprocessing code
-    # for project in projects:
-    #     results.append(get_titles(project))
+    for project in projects:
+        results.append(get_titles_tables(project))
 
     # Multiprocessing Mode - if using, comment out the sequential code
-    with Pool() as pool:
-        results = pool.map(get_titles, projects)
+    # with Pool() as pool:
+    #    results = pool.map(get_titles_tables, projects)
 
     for result in results:
         print(result)
