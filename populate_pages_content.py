ce28be6e66c0905c16687f55ce252d38059f324a
Ivan Duka <ivan.duka.pm@gmail.com>
2020-04-27 14:21:06 -0600

    file rotation

--- populate_pages_content.py
+++ populate_pages_content.py
@@ -9,14 +9,18 @@ import tika
 import time
 from multiprocessing import Pool
 import traceback
+import re
 
 pdf_files_folder_normal = Path("//luxor/data/branch/Environmental Baseline Data/Version 4 - Final/PDF")
-pdf_files_folder_rotated = Path("//luxor/data/branch/Environmental Baseline Data/Version 4 - Final/PDF_rotated")
+pdf_files_folder_rotated90 = Path("//luxor/data/branch/Environmental Baseline Data/Version 4 - Final/PDF_rotated90")
+pdf_files_folder_rotated270 = Path("//luxor/data/branch/Environmental Baseline Data/Version 4 - Final/PDF_rotated270")
 
 if not pdf_files_folder_normal.exists():
     print(pdf_files_folder_normal, "does not exist!")
-if not pdf_files_folder_rotated.exists():
-    print(pdf_files_folder_normal, "does not exist!")
+if not pdf_files_folder_rotated90.exists():
+    print(pdf_files_folder_rotated90, "does not exist!")
+if not pdf_files_folder_rotated270.exists():
+    print(pdf_files_folder_rotated270, "does not exist!")
 
 pd.set_option("display.max_columns", None)
 pd.set_option('display.max_rows', None)
@@ -38,17 +42,17 @@ os.environ["TIKA_SERVER_ENDPOINT"] = "http://127.0.0.1:9998"
 tmp_folder = Path.cwd().joinpath("tmp")
 
 
-def clear_db():
-    stmt1 = "DELETE FROM pages_normal_txt;"
-    stmt2 = "DELETE FROM pages_normal_xml;"
-    stmt3 = "DELETE FROM pages_rotated_txt;"
-    stmt4 = "DELETE FROM pages_rotated_xml;"
-    with engine.connect() as conn:
-        conn.execute(stmt1)
-        conn.execute(stmt2)
-        conn.execute(stmt3)
-        conn.execute(stmt4)
-    print("DB is cleared")
+# def clear_db():
+#     stmt1 = "DELETE FROM pages_normal_txt;"
+#     stmt2 = "DELETE FROM pages_normal_xml;"
+#     stmt3 = "DELETE FROM pages_rotated90_txt;"
+#     stmt4 = "DELETE FROM pages_rotated90_xml;"
+#     with engine.connect() as conn:
+#         conn.execute(stmt1)
+#         conn.execute(stmt2)
+#         conn.execute(stmt3)
+#         conn.execute(stmt4)
+#     print("DB is cleared")
 
 
 def clean_tmp():
@@ -95,7 +99,7 @@ def insert_contents():
     while True:
         try:
             clean_tmp()
-            with Pool() as pool:
+            with Pool(96) as pool:
                 pool.map(insert_content, data, chunksize=1)
         except Exception as e:
             print("\n===========================================================\n")
@@ -116,9 +120,9 @@ def insert_content(row):
 
     pdf_id, total_pages = row["pdfId"], int(row["totalPages"])
     # process_pdf(pdf_id, total_pages, True, True)
-    process_pdf(pdf_id, total_pages, False, True)
-    # process_pdf(pdf_id, total_pages, True, False)
-    # process_pdf(pdf_id, total_pages, False, False)
+    # process_pdf(pdf_id, total_pages, False, True)
+    process_pdf(pdf_id, total_pages, True, False)
+    process_pdf(pdf_id, total_pages, False, False)
 
     # sec = round(time.time() - start_time)
     # print(f"Done {total_pages}x4 in {sec} seconds ({round(sec / 60, 2)} min or {round(sec / 3600, 2)} hours)")
@@ -128,27 +132,26 @@ def process_pdf(pdf_id, pages, xml, normal):
     # print(f"Starting {pdf_id}")
     pdf = pdf_files_folder_normal.joinpath(f"{pdf_id}.pdf")
     if not normal:
-        pdf = pdf_files_folder_rotated.joinpath(f"{pdf_id}.pdf")
+        pdf = pdf_files_folder_rotated90.joinpath(f"{pdf_id}.pdf")
     if not pdf.exists():
         raise Exception(f"{pdf} does not exist! :(")
-    # try:
     with pdf.open(mode="rb") as infile, engine.connect() as conn:
         reader = PyPDF2.PdfFileReader(infile)
         if reader.isEncrypted:
             reader.decrypt("")
         for p in range(1, pages + 1):
-            if normal and xml:
-                check = "SELECT pdfId FROM pages_normal_xml WHERE pdfId = %s AND page_num = %s;"
-                stmt = "INSERT INTO pages_normal_xml (pdfId, page_num, content) VALUES (%s,%s,%s);"
-            if normal and not xml:
-                check = "SELECT pdfId FROM pages_normal_txt WHERE pdfId = %s AND page_num = %s;"
-                stmt = "INSERT INTO pages_normal_txt (pdfId, page_num, content) VALUES (%s,%s,%s);"
+            # if normal and xml:
+            #     check = "SELECT pdfId FROM pages_normal_xml WHERE pdfId = %s AND page_num = %s;"
+            #     stmt = "INSERT INTO pages_normal_xml (pdfId, page_num, content) VALUES (%s,%s,%s);"
+            # if normal and not xml:
+            #     check = "SELECT pdfId FROM pages_normal_txt WHERE pdfId = %s AND page_num = %s;"
+            #     stmt = "INSERT INTO pages_normal_txt (pdfId, page_num, content) VALUES (%s,%s,%s);"
             if not normal and xml:
-                check = "SELECT pdfId FROM pages_rotated_xml WHERE pdfId = %s AND page_num = %s;"
-                stmt = "INSERT INTO pages_rotated_xml (pdfId, page_num, content) VALUES (%s,%s,%s);"
+                check = "SELECT pdfId FROM pages_rotated90_xml WHERE pdfId = %s AND page_num = %s;"
+                stmt = "INSERT INTO pages_rotated90_xml (pdfId, page_num, content) VALUES (%s,%s,%s);"
             if not normal and not xml:
-                check = "SELECT pdfId FROM pages_rotated_txt WHERE pdfId = %s AND page_num = %s;"
-                stmt = "INSERT INTO pages_rotated_txt (pdfId, page_num, content) VALUES (%s,%s,%s);"
+                check = "SELECT pdfId FROM pages_rotated90_txt WHERE pdfId = %s AND page_num = %s;"
+                stmt = "INSERT INTO pages_rotated90_txt (pdfId, page_num, content) VALUES (%s,%s,%s);"
 
             result = conn.execute(check, (pdf_id, p))
             if result.rowcount > 0:
@@ -168,10 +171,69 @@ def process_pdf(pdf_id, pages, xml, normal):
             result = conn.execute(stmt, params)
             if result.rowcount != 1:
                 raise Exception(f"{pdf_id}-{p}: ERROR! Updated {result.rowcount} rows!")
-    # except Exception as e:
-    #     print(f"pdfId {pdf_id} (normal: {normal}): {e}")
+
+
+def clean_text(txt):
+    rgx = re.compile(r'[^\w`~!@#$%^&*()_=+[{}|;:\',<.>/?\-\\\"\]]+')
+    result = re.sub(rgx, " ", txt)
+    return result.strip()
+
+
+def insert_clean_content():
+    t = time.time()
+
+    stmt = text("SELECT pdfId, page_num, content FROM pages_rotated90_txt;")
+    with engine.connect() as conn:
+        df = pd.read_sql(stmt, conn)
+        data = df.to_dict("records")
+        for item in data:
+            cleaned_text = clean_text(item["content"])
+            query = "UPDATE pages_rotated90_txt SET clean_content = %s WHERE pdfId = %s AND page_num = %s"
+            result = conn.execute(query, (cleaned_text, item["pdfId"], item["page_num"]))
+            if result.rowcount != 1:
+                raise Exception(f"{item}: updated {result.rowcount} rows")
+            print(item["pdfId"], item["page_num"], "is done")
+
+    sec = round(time.time() - t)
+    print(f"Done {len(data)} in {sec} seconds ({round(sec / 60, 2)} min or {round(sec / 3600, 2)} hours)")
+
+
+def rotate_pdf(pdf):
+    def rotate(target_pdf, rotation):
+        with pdf.open(mode="rb") as in_file, target_pdf.open(mode="wb") as out_file:
+            reader = PyPDF2.PdfFileReader(in_file)
+            writer = PyPDF2.PdfFileWriter()
+            for p in range(reader.getNumPages()):
+                page = reader.getPage(p)
+                page.rotateClockwise(rotation)
+                writer.addPage(page)
+            writer.write(out_file)
+
+    pdf_path90 = pdf_files_folder_rotated90.joinpath(f"{pdf.stem}.pdf")
+    pdf_path270 = pdf_files_folder_rotated270.joinpath(f"{pdf.stem}.pdf")
+    rotate(pdf_path90, 90)
+    rotate(pdf_path270, 270)
+    print(f"Done {pdf.stem}")
+
+
+def rotate_pdfs():
+    t = time.time()
+
+    pdfs = list(pdf_files_folder_normal.glob("*.pdf"))
+
+    # for pdf_file in pdfs:
+    #     rotate_pdf(pdf_file)
+
+    with Pool() as pool:
+        pool.map(rotate_pdf, pdfs, chunksize=1)
+
+    sec = round(time.time() - t)
+    print(f"Done {len(pdfs)} in {sec} seconds ({round(sec / 60, 2)} min or {round(sec / 3600, 2)} hours)")
 
 
 if __name__ == "__main__":
     # clear_db()  # Careful! Removes all data!
-    insert_contents()
+    # insert_contents()
+    insert_clean_content()
+    # rotate_pdfs()
+    # insert_contents()
